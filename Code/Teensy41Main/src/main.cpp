9164154d80b060cb5d5fa17d8c045698f2c0ecdf
Sean Boerhout <boeseany@gmail.com>
2021-05-31 20:22:27 -0700

    Support for kinematics updates

--- Code/Teensy41Main/src/main.cpp
+++ Code/Teensy41Main/src/main.cpp
@@ -7,44 +7,52 @@
 #include "Ramp.h"
 rampInt myRamp;
 
+#include <DataTransfer.h>
+#include "dataEnums.h"
+
+//Radio Encryption Key
+uint8_t key[] = {0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08};
+
+
+RH_RF69 rf69(RFM69_CS, RFM69_INT);
+Transfer dataBuffer(&rf69, BUFFER_SIZE, key);
 
 uint16_t dynamicAngle;
 uint16_t demandAngle;
 
-Kinematics leg2(2, 60, 90, 120, 135, 55, 90);
+
+// Kinematics leg2(2, 60, 90, 120, 135, 55, 90); // This sets the initial leg position too
+Kinematics leg2(2, DEFAULT_X, DEFAULT_Y, DEFAULT_Z, 60, 120, 55);
 Servo L2M1;
 Servo L2M2;
 Servo L2M3;
 
-float read();
+
+uint8_t parseControllerData(BIT_OR_BYTE bitOrByte, int index);
+
 void calculateGait();
 float sinusoidGaits(int16_t xIn, int type);
 
 void setup() {
   Serial.begin(9600);
   while (!Serial)
+    ;
 
-  pinMode(19, INPUT);
+  dataBuffer.init();
+  Serial.println("initialization complete");
 
-  L2M1.attach(0);
-  L2M2.attach(1);
-  L2M3.attach(2);
-
+  L2M1.attach(9);
+  L2M2.attach(8);
+  L2M3.attach(7);
 
   // write primary positions to servos
-  // leg2.solveFootPosition(100, 0, 177);  
-  // L2M2.writeMicroseconds(leg2.motor2.angleMicros);
-  // L2M3.writeMicroseconds(leg2.motor3.angleMicros);
-  // while (1);
 
+  // leg2.solveFootPosition(DEFAULT_X, DEFAULT_Y, DEFAULT_Z);
   L2M1.writeMicroseconds(leg2.motor1.angleMicros);
   L2M2.writeMicroseconds(leg2.motor2.angleMicros);
   L2M3.writeMicroseconds(leg2.motor3.angleMicros);
 
   delay(3000);
-
-  myRamp.go(-30);
-  myRamp.go(125, 1500, LINEAR, FORTHANDBACK);
 
 }
 
@@ -55,10 +63,16 @@ void loop() {
   static unsigned int amount = 0;
   if (Serial.available()) {
     amount = Serial.parseInt();
+    // leg2.solveFootPosition(0, amount, 177);        // below
+    // leg2.setFootEndpoint(amount, 45, DEFAULT_Z);
+  } 
+      // leg2.solveFootPosition(0, amount, 177);
+
+      leg2.setFootEndpoint(amount, 45, DEFAULT_Z);
+    // leg2.solveFootPosition(map(parseControllerData(BYTE, JOYL_X), 0, 255, -50, 50), 45, 177);
+
+
     // leg2.solveFootPosition(0, amount, 177);
-  }
-    // leg2.solveFootPosition(amount, myRamp.update(), 177);
-    leg2.solveFootPosition(0, amount, 177);
 
 
     // if (millis() % 10 == 0) {
@@ -69,40 +83,43 @@ void loop() {
     //   Serial.println(demandAngle);
     // }
 
-    // L2M1.writeMicroseconds(leg2.motor1.angleMicros);
-    // L2M2.writeMicroseconds(leg2.motor2.angleMicros);
-    // L2M3.writeMicroseconds(leg2.motor3.angleMicros);
+    leg2.updateDynamicFootPosition();
+
+    L2M1.writeMicroseconds(leg2.motor1.dynamicMicros);
+    L2M2.writeMicroseconds(leg2.motor2.dynamicMicros);
+    L2M3.writeMicroseconds(leg2.motor3.dynamicMicros);
+
 
   // write calculated data to servos
-  L2M1.writeMicroseconds(leg2.getDyamicAngle(M1, MILLIS));
-  L2M2.writeMicroseconds(leg2.getDyamicAngle(M2, MILLIS));
-  L2M3.writeMicroseconds(leg2.getDyamicAngle(M3, MILLIS));
+  // L2M1.writeMicroseconds(leg2.getDyamicAngle(M1, MILLIS));
+  // L2M2.writeMicroseconds(leg2.getDyamicAngle(M2, MILLIS));
+  // L2M3.writeMicroseconds(leg2.getDyamicAngle(M3, MILLIS));
 
 }
 
 
-void calculateGait() {
-  static int16_t xIn = -100;
-  static bool forwards = true;
-  int gait = 0;
-  if (micros() % 5000 == 0) {
-    if (forwards == true) {
-      // gait = (75*cos((PI * xIn)/200));
-      gait = (int)sinusoidGaits(xIn, 4);
-      Serial.println(gait);
-      leg2.solveFootPosition(xIn, 0, (177 - gait));
-      xIn += 1; 
-      if (xIn == 101)
-        forwards = false; 
-    }
-    else if (forwards == false) {
-      xIn -= 1;
-      leg2.solveFootPosition(xIn, 0, 177);
-      if (xIn == -100)
-        forwards = true;
-    }
-  }
-}
+// void calculateGait() {
+//   static int16_t xIn = -100;
+//   static bool forwards = true;
+//   int gait = 0;
+//   if (micros() % 5000 == 0) {
+//     if (forwards == true) {
+//       // gait = (75*cos((PI * xIn)/200));
+//       gait = (int)sinusoidGaits(xIn, 4);
+//       Serial.println(gait);
+//       leg2.solveFootPosition(xIn, 0, (177 - gait));
+//       xIn += 1; 
+//       if (xIn == 101)
+//         forwards = false; 
+//     }
+//     else if (forwards == false) {
+//       xIn -= 1;
+//       leg2.solveFootPosition(xIn, 0, 177);
+//       if (xIn == -100)
+//         forwards = true;
+//     }
+//   }
+// }
 
 
 float sinusoidGaits(int16_t xIn, int type) {
@@ -121,22 +138,10 @@ float sinusoidGaits(int16_t xIn, int type) {
 }
 
 
-float read() {
-  int read1 = analogRead(19);
-  delay(1);
-  int read2 = analogRead(19);
-  delay(1);
-  int read3 = analogRead(19);
-  delay(1);
-
-  float result = ((float)read1 + (float)read2 + (float)read3)/3.0;
-
-  return result;
-
+uint8_t parseControllerData(BIT_OR_BYTE bitOrByte, int index) {
+  dataBuffer.receive();
+  return dataBuffer.read(bitOrByte, index);
 }
-
-
-
 
 
 
